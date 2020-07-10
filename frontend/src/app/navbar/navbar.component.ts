ac442f53330cd59d49f2c76839c87367b74584e9
NemanjaKajzer <kajzapfk@gmail.com>
2020-07-10 21:03:57 +0200

    Proxy i navbar

---
+++ frontend/src/app/navbar/navbar.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor(private router: Router) {
+
+  }
+
+  ngOnInit() {
+
+  }
+
+  showRegister() {
+    this.router.navigate(['/register']);
+  }
+
+
+  showLogin() {
+    this.router.navigate(['/login']);
+  }
+
+
+  // showHomeAgent() {
+  //   this.sessionService.report = false;
+  //   this.sessionService.homeAgent = true;
+  // }
+
+  logOut() {
+
+  }
+}
